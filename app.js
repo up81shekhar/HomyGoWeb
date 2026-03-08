5dfd51540809a2e1d56a00b3af88c0b1b99ce030
Shekhar <up81.shekhar@gmail.com>
2026-03-08 08:22:38 +0530

    port problem resolve

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const mongoose = require("mongoose");
 const path = require("path");
